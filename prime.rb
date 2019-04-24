631346005b53efde1eea887ff60bbd0653f20cc0
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-24 19:24:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+require 'pry'
+def prime?(n)
+ 
+  range = (0..n).to_a
+  range.each do |num|
+    if num != 1 || num != n
+      newNum = n/num
+      
+      
+      
+    end
+ 
+
+end
+
+#prime number only divisible by 1 or itself 
+#method takes in an array argument of integers
+#range of 2 to any number / 2 ..none?
+#|i| reassigning array variable 
+#every number in the array has a remainder its prime ex. 22.5
+
+#check if num is negative if it is ==false
+#a.select(&:negative?) 
